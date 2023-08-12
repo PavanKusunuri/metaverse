a30a3aab37ec323014dff07dffa382e30177d842
Pavan Kusnuri <pavantejakusunuri@gmail.com>
2023-08-12 10:57:16 +0530

    Added Explore Component in Metaversus

--- sections/Explore.jsx
+++ sections/Explore.jsx
@@ -7,7 +7,9 @@ import { staggerContainer } from '../utils/motion';
 import { exploreWorlds} from '../constants';
 
 
-const Explore = () => (
+const Explore = () => {
+  const [active, setActive] = useState(3)
+  return(
   <section className={`${styles.paddings}`} id="explore">
     <motion.div
     variants={staggerContainer}
@@ -29,8 +31,11 @@ const Explore = () => (
   />
 ))}
 </div>
+<p className="font-normal text-[16px] leading-[20px] text-white uppercase"> Enter the Metaverse</p>
+<h2 className="mt-[24px] font-semibold sm:text-[32px] text-[24px] text-white">{title}</h2>
+
     </motion.div>
   </section>
 );
-
+}
 export default Explore;
