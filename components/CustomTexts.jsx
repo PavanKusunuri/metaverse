a98e25d8b9c4f93c96011d9cb53646c6ed911b57
Pavan Kusnuri <pavantejakusunuri@gmail.com>
2023-08-12 12:33:34 +0530

    Fixed All Indentation issues in code to avoid build failure

--- components/CustomTexts.jsx
+++ components/CustomTexts.jsx
@@ -1,16 +1,16 @@
 'use client';
 
-import { m, motion } from 'framer-motion';
+import { motion } from 'framer-motion';
 
 import { textContainer, textVariant2 } from '../utils/motion';
 
 export const TypingText = ({ title, textStyles }) => (
-  <motion.p 
-    variants={textContainer} 
+  <motion.p
+    variants={textContainer}
     className={`font-normal text-[14px]
      text-secondary-white ${textStyles}`}
   >
-    {Array.from(title).map((Letter, index)=> (
+    {Array.from(title).map((Letter, index) => (
       <motion.span variants={textVariant2} key={index}>
         {Letter === '' ? '\u00A0' : Letter}
       </motion.span>
@@ -22,13 +22,11 @@ export const TypingText = ({ title, textStyles }) => (
 
 export const TitleText = ({ title, textStyles }) => (
   <motion.h2
-  variants={textVariant2}
-  initial="hidden"
-  whileInView="show"
-  className={`mt-[8px] font-bold md:text-[64px] text-[40px] text-white ${textStyles}`}
+    variants={textVariant2}
+    initial="hidden"
+    whileInView="show"
+    className={`mt-[8px] font-bold md:text-[64px] text-[40px] text-white ${textStyles}`}
   >
-    
     { title }
-    
-    </motion.h2>
+  </motion.h2>
 );
